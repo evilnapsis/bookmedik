13f07856e3bc6cd630a9fff5107e66ebbf7eef5c
unknown <evilnapsis@gmail.com>
2015-09-21 20:53:17 -0700

    created StatusData.php

---
+++ core/modules/index/model/StatusData.php
@@ -0,0 +1,57 @@
+<?php
+class StatusData {
+	public static $tablename = "status";
+
+
+	public function StatusData(){
+		$this->name = "";
+		$this->lastname = "";
+		$this->email = "";
+		$this->password = "";
+		$this->created_at = "NOW()";
+	}
+
+	public function add(){
+		$sql = "insert into status (name) ";
+		$sql .= "value (\"$this->name\")";
+		return Executor::doit($sql);
+	}
+
+	public static function delById($id){
+		$sql = "delete from ".self::$tablename." where id=$id";
+		Executor::doit($sql);
+	}
+	public function del(){
+		$sql = "delete from ".self::$tablename." where id=$this->id";
+		Executor::doit($sql);
+	}
+
+// partiendo de que ya tenemos creado un objecto StatusData previamente utilizamos el contexto
+	public function update(){
+		$sql = "update ".self::$tablename." set name=\"$this->name\" where id=$this->id";
+		Executor::doit($sql);
+	}
+
+	public static function getById($id){
+		$sql = "select * from ".self::$tablename." where id=$id";
+		$query = Executor::doit($sql);
+		return Model::one($query[0],new StatusData());
+	}
+
+	public static function getAll(){
+		$sql = "select * from ".self::$tablename;
+		$query = Executor::doit($sql);
+		return Model::many($query[0],new StatusData());
+
+	}
+	
+	public static function getLike($q){
+		$sql = "select * from ".self::$tablename." where name like '%$q%'";
+		$query = Executor::doit($sql);
+		return Model::many($query[0],new StatusData());
+	}
+
+
+}
+
+?>
