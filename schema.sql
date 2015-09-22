7b11a31d2a8c3c82088f8d1780c287ddf52362f3
unknown <evilnapsis@gmail.com>
2015-09-21 20:52:04 -0700

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,7 @@
+/*
+* BookMedik Database
+* @author Evilnapsis
+*/
 create database bookmedik;
 use bookmedik; 
 
@@ -13,14 +17,22 @@ create table user (
 	created_at datetime
 );
 
+insert into user (username,password,is_admin,is_active,created_at) value ("admin",sha1(md5("admin")),1,1,NOW());
+
+
 create table pacient (
 	id int not null auto_increment primary key,
+	no varchar(50) not null,
 	name varchar(50) not null,
 	lastname varchar(50),
 	email varchar(255) not null,
 	address varchar(255),
 	phone varchar(255),
 	image varchar(255),
+	sick varchar(500),
+	medicaments varchar(500),
+	alergy varchar(500),
+	is_favorite boolean not null default 1,
 	is_active boolean not null default 1,
 	created_at datetime not null
 );
@@ -35,6 +47,7 @@ insert into category (name) value ("Modulo 1");
 
 create table medic (
 	id int not null auto_increment primary key,
+	no varchar(50) not null,
 	name varchar(50) not null,
 	lastname varchar(50),
 	email varchar(255) not null,
@@ -48,8 +61,20 @@ create table medic (
 );
 
 
-insert into user (username,password,is_admin,created_at) value ("admin",sha1(md5("admin")),1,NOW());
 
+create table status (
+	id int not null auto_increment primary key,
+	name varchar(100) not null
+);
+
+insert into status (id,name) values (1,"Pendiente"), (2,"Aplicada"),(3,"No asistio"),(4,"Cancelada");
+
+create table payment (
+	id int not null auto_increment primary key,
+	name varchar(100) not null
+);
+
+insert into payment (id,name) values (1,"No aplicar"), (2,"Pendiente"),(3,"Pagado"),(4,"Anulado");
 
 create table reservation(
 	id int not null auto_increment primary key,
@@ -60,9 +85,16 @@ create table reservation(
 	time_at varchar(50) not null,
 	created_at datetime not null,
 	pacient_id int not null,
+	sick text not null,
+	medicaments text not null,
 	user_id int not null,
 	medic_id int not null,
+	price double,
 	is_web boolean not null default 0,
+	payment_id int not null default 1,
+	foreign key (payment_id) references payment(id),
+	status_id int not null default 1,
+	foreign key (status_id) references status(id),
 	foreign key (user_id) references user(id),
 	foreign key (pacient_id) references pacient(id),
 	foreign key (medic_id) references medic(id)
