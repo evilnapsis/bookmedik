940a2b249d2f7ed27894a8fb07cdc070f09a0ebc
unknown <evilnapsis@gmail.com>
2015-09-21 21:33:28 -0700

    updated view editreservation

--- core/modules/index/view/editreservation/widget-default.php
+++ core/modules/index/view/editreservation/widget-default.php
@@ -2,11 +2,13 @@
 $reservation = ReservationData::getById($_GET["id"]);
 $pacients = PacientData::getAll();
 $medics = MedicData::getAll();
+$statuses = StatusData::getAll();
+$payments = PaymentData::getAll();
 ?>
 <div class="row">
-	<div class="col-md-8">
+	<div class="col-md-10">
 	<h1>Editar Cita</h1>
-	<br>
+  <hr>
 <form class="form-horizontal" role="form" method="post" action="./?action=updatereservation">
   <div class="form-group">
     <label for="inputEmail1" class="col-lg-2 control-label">Asunto</label>
@@ -48,9 +50,59 @@ $medics = MedicData::getAll();
   <div class="form-group">
     <label for="inputEmail1" class="col-lg-2 control-label">Nota</label>
     <div class="col-lg-10">
-    <textarea class="form-control" name="note" placeholder="Nota"></textarea>
+    <textarea class="form-control" name="note" placeholder="Nota"><?php echo $reservation->note;?></textarea>
     </div>
   </div>
+
+    <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Enfermedad</label>
+    <div class="col-lg-10">
+    <textarea class="form-control" name="sick" placeholder="Enfermedad"><?php echo $reservation->sick;?></textarea>
+    </div>
+  </div>
+      <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Sintomas</label>
+    <div class="col-lg-10">
+    <textarea class="form-control" name="symtoms" placeholder="Sintomas"><?php echo $reservation->symtoms;?></textarea>
+    </div>
+  </div>
+        <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Medicamentos</label>
+    <div class="col-lg-10">
+    <textarea class="form-control" name="medicaments" placeholder="Medicamentos"><?php echo $reservation->medicaments;?></textarea>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Estado de la cita</label>
+    <div class="col-lg-10">
+<select name="status_id" class="form-control" required>
+  <?php foreach($statuses as $p):?>
+    <option value="<?php echo $p->id; ?>" <?php if($p->id==$reservation->status_id){ echo "selected"; }?>><?php echo $p->name; ?></option>
+  <?php endforeach; ?>
+</select>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Estado del pago</label>
+    <div class="col-lg-10">
+<select name="payment_id" class="form-control" required>
+  <?php foreach($payments as $p):?>
+    <option value="<?php echo $p->id; ?>" <?php if($p->id==$reservation->payment_id){ echo "selected"; }?>><?php echo $p->name; ?></option>
+  <?php endforeach; ?>
+</select>
+    </div>
+  </div>
+
+    <div class="form-group">
+    <label for="inputEmail1" class="col-lg-2 control-label">Costo</label>
+    <div class="col-lg-10">
+<div class="input-group">
+  <span class="input-group-addon"><i class="fa fa-usd"></i></span>
+  <input type="text" class="form-control" value="<?php echo $reservation->price;?>" name="price" placeholder="Costo">
+</div>
+    </div>
+  </div>
+
   <div class="form-group">
     <div class="col-lg-offset-2 col-lg-10">
     <input type="hidden" name="id" value="<?php echo $reservation->id; ?>">
