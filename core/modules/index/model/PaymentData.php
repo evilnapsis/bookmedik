99f28646262eba6b9e73c024814382dd624ae05c
unknown <evilnapsis@gmail.com>
2015-09-21 20:54:02 -0700

    created PaymentData.php

---
+++ core/modules/index/model/PaymentData.php
@@ -0,0 +1,57 @@
+<?php
+class PaymentData {
+	public static $tablename = "payment";
+
+
+	public function PaymentData(){
+		$this->name = "";
+		$this->lastname = "";
+		$this->email = "";
+		$this->password = "";
+		$this->created_at = "NOW()";
+	}
+
+	public function add(){
+		$sql = "insert into payment (name) ";
+		$sql .= "value (\"$this->name\")";
+		return Executor::doit($sql);
+	}
+
+	public static function delById($id){
+		$sql = "delete from ".self::$tablename." where id=$id";
+		Executor::doit($sql);
+	}
+	public function del(){
+		$sql = "delete from ".self::$tablename." where id=$this->id";
+		Executor::doit($sql);
+	}
+
+// partiendo de que ya tenemos creado un objecto PaymentData previamente utilizamos el contexto
+	public function update(){
+		$sql = "update ".self::$tablename." set name=\"$this->name\" where id=$this->id";
+		Executor::doit($sql);
+	}
+
+	public static function getById($id){
+		$sql = "select * from ".self::$tablename." where id=$id";
+		$query = Executor::doit($sql);
+		return Model::one($query[0],new PaymentData());
+	}
+
+	public static function getAll(){
+		$sql = "select * from ".self::$tablename;
+		$query = Executor::doit($sql);
+		return Model::many($query[0],new PaymentData());
+
+	}
+	
+	public static function getLike($q){
+		$sql = "select * from ".self::$tablename." where name like '%$q%'";
+		$query = Executor::doit($sql);
+		return Model::many($query[0],new PaymentData());
+	}
+
+
+}
+
+?>
