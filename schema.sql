9b670967497a1f52353d19dd8753e462c2f53913
Agustin Ramos <evilnapsis@gmail.com>
2016-03-15 19:07:00 -0600

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -7,11 +7,11 @@ use bookmedik;
 
 create table user (
 	id int not null auto_increment primary key,
-	username varchar(50) not null,
-	name varchar(50) not null,
-	lastname varchar(50) not null,
-	email varchar(255) not null,
-	password varchar(60) not null,
+	username varchar(50),
+	name varchar(50),
+	lastname varchar(50),
+	email varchar(255),
+	password varchar(60),
 	is_active boolean not null default 1,
 	is_admin boolean not null default 0,
 	created_at datetime
@@ -22,12 +22,12 @@ insert into user (username,password,is_admin,is_active,created_at) value ("admin
 
 create table pacient (
 	id int not null auto_increment primary key,
-	no varchar(50) not null,
-	name varchar(50) not null,
+	no varchar(50),
+	name varchar(50),
 	lastname varchar(50),
 	gender varchar(1),
 	day_of_birth date,
-	email varchar(255) not null,
+	email varchar(255),
 	address varchar(255),
 	phone varchar(255),
 	image varchar(255),
@@ -36,12 +36,12 @@ create table pacient (
 	alergy varchar(500),
 	is_favorite boolean not null default 1,
 	is_active boolean not null default 1,
-	created_at datetime not null
+	created_at datetime
 );
 
 create table category (
 	id int not null auto_increment primary key,
-	name varchar(200) not null
+	name varchar(200)
 	);
 
 insert into category (name) value ("Modulo 1");
@@ -49,17 +49,17 @@ insert into category (name) value ("Modulo 1");
 
 create table medic (
 	id int not null auto_increment primary key,
-	no varchar(50) not null,
-	name varchar(50) not null,
+	no varchar(50),
+	name varchar(50),
 	lastname varchar(50),
 	gender varchar(1),
 	day_of_birth date,
-	email varchar(255) not null,
+	email varchar(255),
 	address varchar(255),
 	phone varchar(255),
 	image varchar(255),
 	is_active boolean not null default 1,
-	created_at datetime not null,
+	created_at datetime,
 	category_id int,
 	foreign key (category_id) references category(id)
 );
@@ -68,32 +68,32 @@ create table medic (
 
 create table status (
 	id int not null auto_increment primary key,
-	name varchar(100) not null
+	name varchar(100)
 );
 
 insert into status (id,name) values (1,"Pendiente"), (2,"Aplicada"),(3,"No asistio"),(4,"Cancelada");
 
 create table payment (
 	id int not null auto_increment primary key,
-	name varchar(100) not null
+	name varchar(100)
 );
 
 insert into payment (id,name) values  (1,"Pendiente"),(2,"Pagado"),(3,"Anulado");
 
 create table reservation(
 	id int not null auto_increment primary key,
-	title varchar(100) not null,
-	note text not null,
-	message text not null,
-	date_at varchar(50) not null,
-	time_at varchar(50) not null,
-	created_at datetime not null,
-	pacient_id int not null,
-	symtoms text not null,
-	sick text not null,
-	medicaments text not null,
-	user_id int not null,
-	medic_id int not null,
+	title varchar(100),
+	note text,
+	message text,
+	date_at varchar(50),
+	time_at varchar(50),
+	created_at datetime,
+	pacient_id int,
+	symtoms text,
+	sick text,
+	medicaments text,
+	user_id int,
+	medic_id int,
 	price double,
 	is_web boolean not null default 0,
 	payment_id int not null default 1,
