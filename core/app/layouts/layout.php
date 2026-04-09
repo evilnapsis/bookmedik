90f3f4c911a7a320631681ea58ba54306876dd10
unknown <evilnapsis@gmail.com>
2026-04-08 22:13:00 -0600

    remove breadcrums

--- core/app/layouts/layout.php
+++ core/app/layouts/layout.php
@@ -220,17 +220,7 @@
             </li>
           </ul>
         </div>
-        <div class="header-divider"></div>
-        <div class="container-fluid">
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb my-0 ms-2">
-              <li class="breadcrumb-item">
-                <!-- if breadcrumb is single--><span>Home</span>
-              </li>
-              <li class="breadcrumb-item active"><span>Dashboard</span></li>
-            </ol>
-          </nav>
-        </div>
+
       </header>
       <div class="body flex-grow-1 px-3">
         <div class="container-fluid">
